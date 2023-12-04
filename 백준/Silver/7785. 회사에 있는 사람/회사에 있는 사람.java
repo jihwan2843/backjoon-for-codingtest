e0c4192b0e67ea3d772889937a206ff600a61163
jihwan2843 <115602492+jihwan2843@users.noreply.github.com>
2023-12-04 22:18:13 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 2312 ms, Memory: 103880 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -2,55 +2,28 @@
 
 import java.util.*;
 
-class Member{
-	private String name;
-	private String status;
-	
-	public String getName() {
-		return name;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public Member(String name, String status) {
-		this.name = name;
-		this.status = status;
-	}
-}
-
 class Main {
 		
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
-		Member[] memberList = new Member[N];
-		Member member;
+		Set<String> set = new TreeSet<>();
 		for(int i=0; i<N; i++) {
-			member = new Member(scan.next(), scan.next());
-			memberList[i] = member;
-		}
-		
-		Arrays.sort(memberList, new Comparator<Member>() {
-
-			@Override
-			public int compare(Member o1, Member o2) {
-				// TODO Auto-generated method stub
-				return o2.getName().compareTo(o1.getName());
-			}
+			String name = scan.next();
+			String status = scan.next();
 			
-		});
-		
-		for(int i=0; i<memberList.length-1; i++) {
-			if(!memberList[i].getName().equals(memberList[i+1].getName()) && memberList[i].getStatus().equals("enter")) {
-				System.out.println(memberList[i].getName());
+			if(status.equals("enter")) {
+				set.add(name);
+			}else {
+				set.remove(name);
 			}
 		}
-		
-		if(memberList[memberList.length-1].getStatus().equals("enter")) {
-			System.out.println(memberList[memberList.length-1].getName());
+		String[] ordered = set.toArray(new String[set.size()]);
+		for(int i=ordered.length-1; i>=0; i--) {
+			System.out.println(ordered[i]);
 		}
+	
+	
 		
 		
 	}
