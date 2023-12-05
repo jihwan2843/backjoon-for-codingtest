d6e545b5fb9437a4c3ded012ad62cfb99f9adde2
jihwan2843 <115602492+jihwan2843@users.noreply.github.com>
2023-12-05 22:36:41 +0900

    [Silver II] Title: 좌표 압축, Time: 3812 ms, Memory: 394780 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -10,34 +10,22 @@ class Main {
 	public static void main(String[] args) throws IOException{
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
-		int[][] num = new int[N][2];
+		int[] num = new int[N];
+		Set<Integer> setNum = new TreeSet<>();
 		for(int i=0; i<N; i++) {
-			num[i][0] = scan.nextInt();
-			num[i][1] = i;
+			num[i] = scan.nextInt();
+			setNum.add(num[i]);
 		}
 		
-		Arrays.sort(num, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				// TODO Auto-generated method stub
-				return o1[0] - o2[0];
-			}
-		});
-		
-		int[] compress = new int[N];
+		Map<Integer,Integer> mapNum = new HashMap<>();
 		int index = 0;
-		compress[num[0][1]] = index;
-		for(int i=1; i<N; i++) {
-			if(num[i][0] !=num[i-1][0]) {
-				compress[num[i][1]] = ++index;
-			}else {
-				compress[num[i][1]] = index;
-			}	
+		for(int x : setNum) {
+			mapNum.put(x, index++);
 		}
 		
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
 		for(int i=0; i < N; i++) {
-			bw.write(compress[i] + " ");
+			bw.write(mapNum.get(num[i])+ " ");
 		}
 		bw.newLine();
 		bw.flush();
