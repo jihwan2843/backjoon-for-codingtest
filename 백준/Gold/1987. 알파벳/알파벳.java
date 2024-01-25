1a91f5b385ab9c03a57b7c988eb92cd0dac4f8e4
jihwan2843 <115602492+jihwan2843@users.noreply.github.com>
2024-01-25 21:10:22 +0900

    [Gold IV] Title: 알파벳, Time: 184 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -7,18 +7,21 @@ class Main {
 	static int c;
 	static int[][] board;
 	static boolean[] used;
+	public static int[][] visited;
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		r = scan.nextInt();
 		c = scan.nextInt();
 		board = new int[r][c];
-		used = new boolean[28];
+		used = new boolean[26];
+		visited = new int[r][c];
 		for(int i=0; i<r; i++) {
 			String str = scan.next();
 			for(int j=0; j<c; j++) {
 				board[i][j] = str.charAt(j)-'A';
 			}
 		}
+		visited[0][0] = 1 << board[0][0];
 		used[board[0][0]] = true;
 		System.out.println(recur(0,0));
 		
@@ -26,8 +29,9 @@ class Main {
 		
 	}
 	
-	static int[] nr = {0,1,0,-1};
-	static int[] nc = {-1,0,1,0};
+	static int[] nr = {-1,0,1,0};
+	static int[] nc = {0,1,0,-1};
+	
 	static int recur(int row, int col) {
 		int result = 0;
 		for(int i=0; i<4; i++) {
@@ -35,7 +39,13 @@ class Main {
 			int nextcol = col + nc[i];
 			if(isBorder(nextRow,nextcol)) continue;
 			if(used[board[nextRow][nextcol]]) continue;
+			
+			int visit = 1 << board[nextRow][nextcol];
+			if(visited[nextRow][nextcol] == (visited[row][col] | visit)) continue;
+			
+			
 			used[board[nextRow][nextcol]] = true;
+			visited[nextRow][nextcol] = visited[row][col] | visit;
 			result = Math.max(result, recur(nextRow,nextcol));
 			used[board[nextRow][nextcol]] = false;
 		}
