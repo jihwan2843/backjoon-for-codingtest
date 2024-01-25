4c6f8b70bbc4dc63da0a75d0788a66df7f238031
jihwan2843 <115602492+jihwan2843@users.noreply.github.com>
2024-01-25 20:45:06 +0900

    [Gold IV] Title: 알파벳, Time: 784 ms, Memory: 13664 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -19,35 +19,34 @@ class Main {
 				board[i][j] = str.charAt(j)-'A';
 			}
 		}
-		
-		
 		used[board[0][0]] = true;
 		System.out.println(recur(0,0));
 		
 		
+		
 	}
+	
 	static int[] nr = {0,1,0,-1};
 	static int[] nc = {-1,0,1,0};
 	static int recur(int row, int col) {
 		int result = 0;
 		for(int i=0; i<4; i++) {
-			int nextrow = row + nr[i];
+			int nextRow = row + nr[i];
 			int nextcol = col + nc[i];
-			if(isboard(nextrow,nextcol)) continue;
-			int next = board[nextrow][nextcol];
-			if(used[next]) continue;
-			used[next] = true;
-			result = Math.max(result, recur(nextrow,nextcol));
-			used[next] = false;
-			
+			if(isBorder(nextRow,nextcol)) continue;
+			if(used[board[nextRow][nextcol]]) continue;
+			used[board[nextRow][nextcol]] = true;
+			result = Math.max(result, recur(nextRow,nextcol));
+			used[board[nextRow][nextcol]] = false;
 		}
 		return result +1;
-			
+	
 	}
-	static boolean isboard(int row, int col) {
-		if(row<0 || col<0 || row>=r || col >=c) {
+	public static boolean isBorder(int row, int col) {
+		if(row<0|| col<0|| row>=r|| col>=c) {
 			return true;
 		}
 		return false;
 	}
+	
 }
